720a61cca3c7ca6b874bee77e8a21c7031ed4e87
200Alex02 <125561173+200Alex02@users.noreply.github.com>
2023-05-27 14:17:47 +0300

    Update PlantAdapter.kt

--- PlantAdapter.kt
+++ PlantAdapter.kt
@@ -13,14 +13,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recycler_view.databinding.ItemPlantsBinding
 
-class PlantAdapter() :
+class PlantAdapter(private val onItemClick: (position: Int) -> Unit) :
     ListAdapter<Plant, PlantAdapter.PlantHolder>(DiffCallback()) {
 
-    class PlantHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class PlantHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val binding = ItemPlantsBinding.bind(itemView)
 
-        fun bind(plant: Plant) = with(binding) {
+        fun bind(plant: Plant, index: Int) = with(binding) {
             namePlant.text = plant.name
             typeOfPlant.text = plant.type
             leafColor.text = plant.color
@@ -30,29 +30,46 @@ class PlantAdapter() :
                 .error(R.drawable.error)
                 .placeholder(R.drawable.error)
                 .into(imageView)
+
         }
 
     }
 
-    private class DiffCallback : DiffUtil.ItemCallback<Plant>() {
-        override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
-            return oldItem.photo == newItem.photo
-        }
-
-        override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
-            return oldItem == newItem
-        }
-
-    }
+    /*private fun deleteItem(position: Int) {
+        val newList = currentList.toMutableList()
+        newList.removeAt(position)
+        submitList(newList)
+    }*/
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantHolder {
         val itemView =
             LayoutInflater.from(parent.context).inflate(R.layout.item_plants, parent, false)
-        return PlantHolder(itemView)
+        return PlantHolder(itemView).apply {
+            itemView.setOnClickListener {
+                val position = bindingAdapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    onItemClick(position)
+                }
+            }
+
+        }
+
     }
 
     override fun onBindViewHolder(holder: PlantHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(getItem(position), position)
+
+    }
+
+}
+
+private class DiffCallback : DiffUtil.ItemCallback<Plant>() {
+    override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem.photo == newItem.photo
+    }
+
+    override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem == newItem
     }
 
 }
